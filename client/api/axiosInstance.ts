55293352d11cee859e27905a15784366326b2a69
chamikarab <chamikara38@gmail.com>
2025-04-03 00:03:44 +0530

    Deleted : Node Modules

--- client/api/axiosInstance.ts
+++ client/api/axiosInstance.ts
@@ -1,11 +1,7 @@
 // client/api/axiosInstance.ts
 import axios from 'axios';
 
-<<<<<<< HEAD
 const baseURL = 'http://192.168.1.5:3001';
-=======
-const baseURL = 'http://192.168.1.23:9090';
->>>>>>> af2d749b6ad0dc96ab6e187d46d242a1c3502fa5
 
 const axiosInstance = axios.create({
   baseURL,
